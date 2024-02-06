68ac9edd6c27ea83831043ddcebbc086af86cbdd
vsiago <vsiago23@gmail.com>
2024-02-06 07:52:37 -0300

    Feat: Feat: Logica filtro inicial ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,32 +17,16 @@ export default function Home() {
   ]);
 
   const [statuses, setStatuses] = useState(Array(chamados.length).fill("Recebido"));
-  const [concluidosCount, setConcluidosCount] = useState(0);
-  const [emAndamentoCount, setEmAndamentoCount] = useState(0);
-  const [emAnaliseCount, setEmAnaliseCount] = useState(0);
-  const [recusadosCount, setRecusadosCount] = useState(0);
+  const [filtro, setFiltro] = useState('Todos');
 
   useEffect(() => {
     document.title = 'Chamados Prefeitura';
   }, []);
 
   useEffect(() => {
-    // Atualizar o contador de chamados concluídos
-    const concluidos = chamados.filter(chamado => chamado.status === 'Concluído');
-    setConcluidosCount(concluidos.length);
-
-    // Atualizar o contador de chamados em andamento
-    const emAndamento = chamados.filter(chamado => chamado.status === 'Em andamento');
-    setEmAndamentoCount(emAndamento.length);
-
-    // Atualizar o contador de chamados em análise
-    const emAnalise = chamados.filter(chamado => chamado.status === 'Em análise');
-    setEmAnaliseCount(emAnalise.length);
-
-    // Atualizar o contador de chamados recusados
-    const recusados = chamados.filter(chamado => chamado.status === 'Rejeitado');
-    setRecusadosCount(recusados.length);
-  }, [chamados]);
+    // Atualizar o estado dos chamados
+    setChamados((prevChamados) => prevChamados.map((chamado, index) => ({ ...chamado, status: statuses[index] })));
+  }, [statuses]);
 
   const handleOpenModal = (index: any) => {
     setOpenModalIndex(index);
@@ -52,28 +36,14 @@ export default function Home() {
     setOpenModalIndex(null);
   }
 
-  const showConcluidos = () => {
-    const allConcluidos = chamados.filter((item) => item.status === 'Concluído');
-    console.log(allConcluidos);
-    console.log('Clicou em Concluídos');
+  const handleFiltroClick = (filtro: string) => {
+    setFiltro(filtro === 'Todos' ? 'Todos' : filtro);
   };
 
-  const showEmAndamento = () => {
-    const allEmAndamento = chamados.filter((item) => item.status === 'Em andamento');
-    console.log(allEmAndamento);
-    console.log('Clicou em Em andamento');
-  };
-
-  const showEmAnalise = () => {
-    const allEmAnalise = chamados.filter((item) => item.status === 'Em análise');
-    console.log(allEmAnalise);
-    console.log('Clicou em Em análise');
-  };
-
-  const showRecusados = () => {
-    const allRecusados = chamados.filter((item) => item.status === 'Rejeitado');
-    console.log(allRecusados);
-    console.log('Clicou em Recusados');
+  const handleSetStatus = (index: any, newStatus: any) => {
+    const newStatuses = [...statuses];
+    newStatuses[index] = newStatus;
+    setStatuses(newStatuses);
   };
 
   const statusColors: Record<string, string> = {
@@ -85,21 +55,12 @@ export default function Home() {
     // Adicione outras cores conforme necessário
   };
 
-  const handleSetStatus = (index: any, newStatus: any) => {
-    const newStatuses = [...statuses];
-    newStatuses[index] = newStatus;
-    setStatuses(newStatuses);
-
-    // Atualizar o status no objeto chamado correspondente
-    const updatedChamados = chamados.map((chamado, i) => {
-      if (i === index) {
-        return { ...chamado, status: newStatus };
-      }
-      return chamado;
-    });
-
-    // Atualizar o estado dos chamados
-    setChamados(updatedChamados);
+  const filtroFunc: Record<string, (chamado: any) => boolean> = {
+    Todos: () => true,
+    Concluidos: (chamado) => chamado.status === 'Concluído',
+    EmAndamento: (chamado) => chamado.status === 'Em andamento',
+    EmAnalise: (chamado) => chamado.status === 'Em análise',
+    Recusados: (chamado) => chamado.status === 'Rejeitado'
   };
 
   return (
@@ -120,11 +81,11 @@ export default function Home() {
         <div className="flex justify-between items-center h-40">
           <h1 className="text-4xl text-gray-600 ">Chamados</h1>
           <div className="flex gap-6 items-end">
-            <div className="flex items-center gap-3 px-6 py-3 rounded-md hover:bg-slate-100 hover:cursor-pointer hover:shadow-sm hover:transition-all duration-200">
+            <div className="flex items-center gap-3 px-6 py-3 rounded-md hover:bg-slate-100 hover:cursor-pointer hover:shadow-sm hover:transition-all duration-200" onClick={() => handleFiltroClick('Todos')}>
               <p className="text-slate-600 font-[600] text-end leading-5">Todos os<br></br> chamados:</p>
               <p className="text-slate-600 text-5xl">{chamados.length || 0}</p>
             </div>
-            <div className="flex flex-col items-center hover:bg-green-50 px-4 py-2 rounded-md hover:shadow-sm hover:cursor-pointer" onClick={() => showConcluidos()}>
+            <div className="flex flex-col items-center hover:bg-green-50 px-4 py-2 rounded-md hover:shadow-sm hover:cursor-pointer" onClick={() => handleFiltroClick('Concluidos')}>
               <div className="flex items-center">
                 <Image
                   src="./icons/check.svg"
@@ -133,11 +94,11 @@ export default function Home() {
                   height={43}
                   priority
                 />
-                <p className="text-slate-600 text-2xl leading-6">{concluidosCount}</p>
+                <p className="text-slate-600 text-2xl leading-6">{statuses.filter(status => status === 'Concluído').length}</p>
               </div>
               <p className="text-slate-600 text-sm font-medium mt-[-4px]">Concluídos</p>
             </div>
-            <div className="flex flex-col items-center hover:bg-sky-50 px-4 py-2 rounded-md hover:shadow-sm hover:cursor-pointer" onClick={() => showEmAndamento()}>
+            <div className="flex flex-col items-center hover:bg-sky-50 px-4 py-2 rounded-md hover:shadow-sm hover:cursor-pointer" onClick={() => handleFiltroClick('EmAndamento')}>
               <div className="flex items-center">
                 <Image
                   src="./icons/progress.svg"
@@ -146,11 +107,11 @@ export default function Home() {
                   height={38}
                   priority
                 />
-                <p className="text-slate-600 text-2xl leading-6">{emAndamentoCount}</p>
+                <p className="text-slate-600 text-2xl leading-6">{statuses.filter(status => status === 'Em andamento').length}</p>
               </div>
               <p className="text-slate-600 text-sm font-medium mt-[-3px]">Em andamento</p>
             </div>
-            <div className="flex flex-col items-center hover:bg-yellow-50 px-4 py-2 rounded-md hover:shadow-sm hover:cursor-pointer" onClick={() => showEmAnalise()}>
+            <div className="flex flex-col items-center hover:bg-yellow-50 px-4 py-2 rounded-md hover:shadow-sm hover:cursor-pointer" onClick={() => handleFiltroClick('EmAnalise')}>
               <div className="flex items-center">
                 <Image
                   src="./icons/search.svg"
@@ -159,11 +120,11 @@ export default function Home() {
                   height={38}
                   priority
                 />
-                <p className="text-slate-600 text-2xl leading-6">{emAnaliseCount}</p>
+                <p className="text-slate-600 text-2xl leading-6">{statuses.filter(status => status === 'Em análise').length}</p>
               </div>
               <p className="text-slate-600 text-sm font-medium mt-[-3px]">Em Análise</p>
             </div>
-            <div className="flex flex-col items-center hover:bg-red-50 px-4 py-2 rounded-md hover:shadow-sm hover:cursor-pointer" onClick={() => showRecusados()}>
+            <div className="flex flex-col items-center hover:bg-red-50 px-4 py-2 rounded-md hover:shadow-sm hover:cursor-pointer" onClick={() => handleFiltroClick('Recusados')}>
               <div className="flex items-center">
                 <Image
                   src="./icons/reject.svg"
@@ -172,14 +133,14 @@ export default function Home() {
                   height={38}
                   priority
                 />
-                <p className="text-slate-600 text-2xl leading-6">{recusadosCount}</p>
+                <p className="text-slate-600 text-2xl leading-6">{statuses.filter(status => status === 'Rejeitado').length}</p>
               </div>
               <p className="text-slate-600 text-sm font-medium mt-[-3px]">Recusados</p>
             </div>
           </div>
         </div>
         <ul>
-          {chamados.map((chamado, index) => (
+          {chamados.filter(filtroFunc[filtro]).map((chamado, index) => (
             <li key={index} className="bg-slate-100 min-h-4 rounded grid justify-between grid-cols-7 p-2 my-1 cursor-pointer transition-all ease-in-out duration-200 hover:bg-slate-50 hover:shadow-sm">
               <div className="flex col-span-2 justify-center w-full items-start flex-col">
                 <p className="text-gray-500 uppercase text-xs tracking-wider">ASSUNTO do chamado</p>
@@ -187,7 +148,6 @@ export default function Home() {
               </div>
               <div className="flex justify-center w-full items-start flex-col">
                 <p className="text-gray-500 uppercase text-xs tracking-wider">Secretaria</p>
-
                 <p className="text-gray-700">{chamado.secretaria}</p>
               </div>
               <div className="col-span-1 flex justify-center items-start flex-col">
@@ -200,9 +160,7 @@ export default function Home() {
               </div>
               <div className="flex justify-center w-4/6 items-start flex-col">
                 <p className="text-gray-500 uppercase text-xs tracking-wider">Status</p>
-                <p className={statusColors[statuses[index] || chamados[index].status] || 'text-gray-600'}>
-                  {statuses[index] ? statuses[index] : chamados[index].status ? chamados[index].status : 'Recebido'}
-                </p>
+                <p className={statusColors[chamado.status]}>{chamado.status}</p>
               </div>
               <div className="cursor-pointer relative flex items-center justify-end col-span-1">
                 <div className="p-4 w-2" onMouseEnter={() => handleOpenModal(index)} onMouseLeave={handleCloseModal}>
